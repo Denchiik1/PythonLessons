ed8e1e612b1fd5dddd2849d0ae9cbf55dbce1191
Даниил <danya.45san@outlook.com>
2022-11-15 16:52:39 +0300

    Принт строки без двойных а.

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 s = input()
-mas = "0123456789"
-mas2 = "qwertyuiopasdfghjklzxcvbnm"
-d = ""
-i = 0
-while i < len(s):
-    if s[i] == 'a' and s[i+1] == 'a':
-        d += '*'
-        i += 2
-    else:
-        d += s[i]
-        i += 1
-print(d)
+s = s.replace('aa', '*')
+print(s)
