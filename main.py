acd7252f2bd4a796e821cdce7cc8b414fdb7a9d7
Даниил <danya.45san@outlook.com>
2022-11-15 17:37:57 +0300

    True or False разнообразной строки.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 s = input()
-s = s.replace('aa', '*')
-print(s)
+def f(s):
+    m = set()
+    for x in s:
+        m.add(x)
+    for i in s:
+        if s.count(i) > len(m):
+            return False
+        else:
+            return True
+print(f(s))
