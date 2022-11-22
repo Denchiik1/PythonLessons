bb746ea558c322b5cc42b51c73beb367ecc1ccdc
Даниил <danya.45san@outlook.com>
2022-11-22 17:10:57 +0300

    Количество идущих подряд неодинаковых букв.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-s = '12345'
-l = 1
-for i in range(0, len(s) - (l - 1)):
-    print(s[i: i + l])
-for i in range(0, len(s) - l):
-    print(s[i: i+ (l + 1)])
-for i in range(0,  len(s) - (l + 1)):
-    print(s[i: i + (l + 2)])
-for i in range(0, len(s) - (l + 2)):
-    print(s[i: i + (l + 3)])
-for i in range(0, len(s) - (l + 3)):
-    print(s[i: i + (l + 4)])
+s1 = open("C:\\Users\\danya\\OneDrive\\Документы\\24.txt")
+s = s1.read()
+s1.close()
+l = []
+i = 0
+k = 1
+for j in range(1, len(s)):
+    if s[j] != s[j - 1]:
+        k += 1
+    else:
+        l.append(j - i)
+        i = j
+print(max(l))
