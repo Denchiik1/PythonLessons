648802d0ea92d6ab344a384cd0c9291401b055e6
Даниил <danya.45san@outlook.com>
2022-11-22 18:09:05 +0300

    Расчет самого большого числа без max() в массиве.

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-s1 = open("C:\\Users\\danya\\OneDrive\\Документы\\24.txt")
-s = s1.read()
-s1.close()
-l = []
-i = 0
-k = 1
-for j in range(1, len(s)):
-    if s[j] != s[j - 1]:
-        k += 1
-    else:
-        l.append(j - i)
-        i = j
-print(max(l))
+mas = [1, 2, 2, 3]
+j = mas[0]
+for i in range(1, len(mas)):
+    if i > j:
+        j = mas[i]
+print(j)
