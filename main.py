09af70d964f4d8b4aa943af24be57758b8c7d3ea
Даниил <danya.45san@outlook.com>
2022-11-29 17:15:12 +0300

    Строка из текста с наибольшим количеством букв G.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-mas = [1, 2, 2, 3]
-j = mas[0]
-for i in range(1, len(mas)):
-    if i > j:
-        j = mas[i]
-print(j)
+s1 = open("C:\\Users\\danya\\OneDrive\\Изображения\\Telegram\\inf24.txt")
+s = s1.readlines()
+s1.close()
+l = []
+k = 0
+for j in s:
+    l.append(j.count("G"))
+m = l[0]
+for i in range(len(l)):
+    if l[i] > m:
+        m = l[i]
+        k = i
+print(s[k])
