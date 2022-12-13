4642530c233115b16959c72cbf2cd785abcc2479
Даниил <danya.45san@outlook.com>
2022-12-13 17:56:34 +0300

    Принт чисел в определенном диапазоне, имеющих только 2 делителя.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-for a in range(174457, 174505):
-    e = []
-    for i in range(2, a):
-        if a % i == 0:
-            e.append(i)
-    if len(e) == 2:
-        print(e[0], e[1], sep='  ')
+f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\25.txt")
+s = f.readlines()
+f.close()
+d = s[0].split()
+d = int(d[0])
+l = []
+i = 0
+summa = 0
+last = 0
+for k in range(1, len(s)):
+    l.append(int(s[k]))
+l.sort()
+summ = 0
+while summ < d:
+    i += 1
+    summ = sum(l[:i])
+print(d, i)
+summ = sum(l[:i - 1])
+k = i - 2
+t = i - 1
+summ = summ - l[k] + l[t]
+while summ < d:
+    k += 1
+    t += 1
+    summ = summ - l[k] + l[t]
+print(l[k])
