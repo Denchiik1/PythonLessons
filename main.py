f2176b479feae0f979d3904750e9312d734c7166
Даниил <danya.45san@outlook.com>
2022-12-27 17:51:04 +0300

    Вывод значений x и y из файла, которые находятся в первой части плоскости.

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from math import sqrt
-
-f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\Programm3.txt")
-s = f.read()
+f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\Programm4.txt")
+s = f.readlines()
 f.close()
 l = []
-srednee_arif_count = 0
-srednee_arif = 0
-D = 0
-a = s.split()
-for x in a:
-    l.append(int(x))
+a = 0
+for x in s:
+    a = x.split()
+    l.append(a)
 for i in l:
-    srednee_arif += i
-    srednee_arif_count += 1
-summa = srednee_arif
-srednee_arif = srednee_arif / srednee_arif_count
-for j in l:
-    D += ((j - srednee_arif) ** 2) / len(l)
-zabil_nazvanie_bukvi = sqrt(D)
-print(zabil_nazvanie_bukvi)
+    if int(i[0]) >= 1 and int(i[1]) >= 1:
+        print(i[0], i[1])
