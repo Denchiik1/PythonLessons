c65b41e875006374c8e7abe5dcd2d68b59111ac9
Даниил <danya.45san@outlook.com>
2022-12-27 16:44:53 +0300

    Вывод суммы определенных товаров.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\NewProgramm1.txt")
+f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\Programm2.txt")
 s = f.readlines()
 f.close()
 l = []
-for k in s:
-    l.append(int(k))
-j = l[0]
-for i in range(0, len(l)):
-    if l[i] < j:
-        j = l[i]
-print(j)
+for x in s:
+    a = x.split()
+    if a[2] == "1":
+        l.append(a[0])
+        l.append(a[1])
+summa = 0
+for i in range(0, len(l), 2):
+        t = int(l[i]) * int(l[i + 1])
+        summa += t
+print(summa)
