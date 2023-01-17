2748d3ee4f7bb8789119bc983cf78564ac7f8802
Даниил <danya.45san@outlook.com>
2023-01-17 17:55:21 +0300

    Задание с ЕГЭ с Исполнителем Черепахой(квадрат).

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from math import sqrt
 count = 0
-b1 = 10
-k1 = -1 / sqrt(3)
-b2 = 0
-k2 = 1 / sqrt(3)
-tyash_chislo = (10 * sqrt(3)) / 2
-for x in range(1, 809):
+b = 10
+k = 0
+for x in range(1, 10):
     for y in range(1, 10):
-        if y > k2 * x + b2 and y < k1 * x + b1:
+        if y < 10:
             count += 1
 print(count)
