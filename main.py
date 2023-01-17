151e1242146502514b3dc06507ec69b5654e76f0
Даниил <danya.45san@outlook.com>
2023-01-17 16:53:50 +0300

    Все точки находящиеся в треугольнике и не касающиеся его краев.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-R = 5
 n = 0
-for x in range(-4, 5):
-    for y in range(-4, 5):
-        if x ** 2 + y ** 2 < R ** 2:
-            n += 1
-            print(x, y, n)
+k1 = 1.5
+b1 = 0
+b2 = 4.5
+k2 = -0.75
+count = 0
+for x in range(1, 3):
+    for y in range(1, 3):
+        if k1 * x + b1 > y:
+            print(x, y)
+            count += 1
+for x in range(3, 6):
+    for y in range(1, 3):
+        if k2 * x + b2 > y:
+            print(x, y)
+            count += 1
+print(count)
