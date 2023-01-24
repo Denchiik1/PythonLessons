c4e740d93fbd52e62b7ab6ad3e2aee27d07cef8e
Даниил <danya.45san@outlook.com>
2023-01-24 17:11:08 +0300

    Точки находящиеся в квадрате со сторой но при этом не находящиеся в треугольнике с такими же сторонами.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-s = input()
-smas = s.split()
-mas = [int(x) for x in smas]
-l = [x for i, x in enumerate(mas) if i % 3 > 0]
-print(l)
+count = 0
+count1 = 0
+count2 = 0
+b1 = 12
+k1 = -1/3**0.5
+b2 = 0
+k2 = 1/3**0.5
+
+for x in range(1, 12):
+    for y in range(1, 12):
+        if not (k1 * x + b1 > y and k2 * x + b2 < y):
+            count += 1
+
+print(count)
