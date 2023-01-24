6612ce24a7d28db8350071f8d657375ea7fc475b
Даниил <danya.45san@outlook.com>
2023-01-24 17:50:07 +0300

    Слова состоящие из букв A, B, C, X при этом первая буква равна X и остальные не равны ей или X не появляется вообще.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 l = []
 count = 0
-abc = "ЕГЭ"
+abc = "ABCX"
 for x1 in abc:
-    for x2 in abc:
-        for x3 in abc:
-            for x4 in abc:
-                for x5 in abc:
-                    if x1 in "ЕЭ":
-                        s = x1 + x2 + x3 + x4 + x5
-                        l.append(s)
-                        count += 1
+    if x1 == "X":
+        for x2 in abc:
+            for x3 in abc:
+                for x4 in abc:
+                    for x5 in abc:
+                        if x2 != "X" and x3 != "X" and x4 != "X" and x5 != "X":
+                                s = x1 + x2 + x3 + x4 + x5
+                                l.append(s)
+                                count += 1
+    else:
+        for x2 in abc:
+            for x3 in abc:
+                for x4 in abc:
+                    for x5 in abc:
+                        if x2 != "X" and x3 != "X" and x4 != "X" and x5 != "X":
+                            s = x1 + x2 + x3 + x4 + x5
+                            l.append(s)
+                            count += 1
 print(count)
