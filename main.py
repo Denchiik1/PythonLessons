6e8cf9dc9a019ad54355f06d8e9a3bb10c680693
Даниил <danya.45san@outlook.com>
2023-01-24 17:29:00 +0300

    Слова состоящие только из букв Е, Г и Э

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+l = []
 count = 0
-count1 = 0
-count2 = 0
-b1 = 12
-k1 = -1/3**0.5
-b2 = 0
-k2 = 1/3**0.5
-
-for x in range(1, 12):
-    for y in range(1, 12):
-        if not (k1 * x + b1 > y and k2 * x + b2 < y):
-            count += 1
-
+abc = "ЕГЭ"
+for x1 in abc:
+    for x2 in abc:
+        for x3 in abc:
+            for x4 in abc:
+                for x5 in abc:
+                    if x1 in "ЕЭ":
+                        s = x1 + x2 + x3 + x4 + x5
+                        l.append(s)
+                        count += 1
 print(count)
