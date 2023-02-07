b2f715419fb3d06ef9c8b9af75da87cb77d9a4bb
Даниил <danya.45san@outlook.com>
2023-02-07 16:51:40 +0300

    Подсчет цифр 3 в числе.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-s = '9' * 85
-while ('666' in s) or ('999' in s):
-    if '666' in s:
-        s = s.replace('666', '9', 1)
+def F(n):
+    if n == 0:
+        return 0
+    elif n % 2 == 0:
+        return F(n // 2)
     else:
-        s = s.replace('999', '6', 1)
-print(s)
+        return 1 + F(n - 1)
+l = []
+for x in range(1, 1001):
+    if F(x) == 3:
+        l.append(x)
+print(len(l))
