3b257e08e40b2312a7ea88e7b7ecea3933db8d2e
Даниил <danya.45san@outlook.com>
2023-02-07 16:58:00 +0300

    Алгоритм вычисления значения функции F(n), где n  — целое неотрицательное число, задан следующими соотношениями:
    F(n) = 0, при n ≤ 1;
    F(n) = F(n − 1) + 3n**2, если n > 1 и при этом нечётно;
    F(n) = n / 2 + F(n − 1) + 2, если n > 1 и при этом чётно.
    
    Чему равно значение функции F(49)?

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 def F(n):
-    if n == 0:
+    if n <= 1:
         return 0
-    elif n % 2 == 0:
-        return F(n // 2)
+    elif n > 1 and n % 2 != 0:
+        return F(n - 1) + 3 * n ** 2
     else:
-        return 1 + F(n - 1)
-l = []
-for x in range(1, 1001):
-    if F(x) == 3:
-        l.append(x)
-print(len(l))
+        return n / 2 + F(n - 1) + 2
+print(int(F(49)))
