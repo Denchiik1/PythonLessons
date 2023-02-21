8b64ed4b67e32349927131c5c92ce9355c4d5214
Даниил <danya.45san@outlook.com>
2023-02-21 17:43:24 +0300

    Текстовый файл содержит строки различной длины. Общий объём файла не превышает 1 Мбайт. Строки содержат только заглавные буквы латинского алфавита (ABC…Z). Определите количество строк, в которых буква E встречается чаще, чем буква A. Для выполнения этого задания следует написать программу. Ниже приведён файл, который необходимо обработать с помощью данного алгоритма

--- main 4.py
+++ main 4.py
@@ -1,3 +1,8 @@
-from math import *
-factorial(5)
-print(factorial(5))
+f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\inf_22_10_20_24.txt")
+s = f.readlines()
+f.close()
+k = 0
+for x in s:
+    if x.count("E") > x.count("A"):
+        k += 1
+print(k)
