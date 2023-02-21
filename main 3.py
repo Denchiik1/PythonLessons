b3e3dc90b1e60ab135b337e15ecb6632693e5299
Даниил <danya.45san@outlook.com>
2023-02-21 17:34:24 +0300

    Текстовый файл состоит не более чем из 106 символов A, B и C. Определите максимальное количество идущих подряд символов A. Для выполнения этого задания следует написать программу. Ниже приведён файл, который необходимо обработать с помощью данного алгоритма.

--- main 3.py
+++ main 3.py
@@ -1,4 +1,12 @@
-d = {}
-for i in input().split():
-    d[i] = d.get(i, 0) + 1
-    print(d[i], end=' ')
+f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\21.02.2023_3.txt")
+s = f.read()
+f.close()
+m = 0
+k = 1
+for x in range(0, len(s) - 1):
+    if s[x] == "A" and s[x + 1] == "A":
+        k += 1
+    else:
+        m = max(k, m)
+        k = 1
+print(m)
