7573b9056b0ffa3e92a009010c55e57ba5358c4d
Даниил <danya.45san@outlook.com>
2023-02-28 17:33:50 +0300

    В файле содержится последовательность из 10 000 натуральных чисел. Каждое число не превышает 10 000. Определите и запишите в ответе сначала количество пар элементов последовательности, у которых различные остатки от деления на d  =  160 и хотя бы одно из чисел делится на p  =  7, затем максимальную из сумм элементов таких пар. В данной задаче под парой подразумевается два различных элемента последовательности. Порядок элементов в паре не важен. Пример входных данных: 168 7 320 328 Пример выходных данных для приведённого выше примера входных данных: 4 488 https://inf-ege.sdamgia.ru/get_file?id=91147

--- main 2.py
+++ main 2.py
@@ -1,16 +1,17 @@
-f = open("C:\\Users\\danya\\Downloads\\17.txt")
+f = open("C:\\Users\\danya\\Downloads\\17 (1).txt")
 s = f.read()
 f.close()
 s = s.split()
 summ_par = 0
 maks_znach = 0
 l = [int(x) for x in s]
-for i in range(len(l) - 1):
-    x = l[i]
-    y = l[i + 1]
-    if x % 3 == 0 or y % 3 == 0:
-        summ_par += 1
-    if (x + y) > maks_znach:
-        maks_znach = (x + y)
+for i in range(len(l)):
+    for j in range(i + 1, len(l)):
+        x = l[i]
+        y = l[j]
+        if (x % 160 != y % 160) and (x % 7 == 0 or y % 7 == 0):
+            summ_par += 1
+            if x + y > maks_znach:
+                maks_znach = x + y
 print(summ_par)
 print(maks_znach)
