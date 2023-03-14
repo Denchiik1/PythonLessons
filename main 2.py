4b1a0aa74dfd4414bf5cfd9f8279b035fdeaf06c
Даниил <danya.45san@outlook.com>
2023-03-14 19:30:22 +0300

    Напишите программу, которая ищет среди целых чисел, принадлежащих числовому отрезку [174457; 174505], числа, имеющие ровно два различных натуральных делителя, не считая единицы и самого числа. Для каждого найденного числа запишите эти два делителя в два соседних столбца на экране с новой строки в порядке возрастания произведения этих двух делителей. Делители в строке также должны следовать в порядке возрастания. Например, в диапазоне [5; 9] ровно два различных натуральных делителя имеют числа 6 и 8, поэтому для этого диапазона вывод на экране должна содержать следующие значения: 2 3 2 4

--- main 2.py
+++ main 2.py
@@ -1,17 +1,7 @@
-f = open("C:\\Users\\danya\\Downloads\\17 (1).txt")
-s = f.read()
-f.close()
-s = s.split()
-summ_par = 0
-maks_znach = 0
-l = [int(x) for x in s]
-for i in range(len(l)):
-    for j in range(i + 1, len(l)):
-        x = l[i]
-        y = l[j]
-        if (x % 160 != y % 160) and (x % 7 == 0 or y % 7 == 0):
-            summ_par += 1
-            if x + y > maks_znach:
-                maks_znach = x + y
-print(summ_par)
-print(maks_znach)
+for n in range(174457, 174505):
+    l = []
+    for delit in range(2, n):
+        if n % delit == 0:
+            l.append(delit)
+    if len(l) == 2:
+            print(l[0], l[1])
