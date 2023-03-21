f42b211740fd13ee71d6f93bb84c922f68036593
Даниил <danya.45san@outlook.com>
2023-03-21 19:04:22 +0300

    Напишите программу, которая ищет среди целых чисел, принадлежащих числовому отрезку [110203; 110245], числа, имеющие ровно четыре различных чётных натуральных делителя (при этом количество нечётных делителей может быть любым). Для каждого найденного числа запишите эти четыре делителя в четыре соседних столбца на экране с новой строки. Делители в строке должны следовать в порядке возрастания. Например, в диапазоне [2; 16] ровно четыре чётных различных натуральных делителя имеют числа 12 и 16, поэтому для этого диапазона вывод на экране должна содержать следующие значения: 2 4 6 12 2 4 8 16

--- main 3.py
+++ main 3.py
@@ -1,12 +1,7 @@
-f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\21.02.2023_3.txt")
-s = f.read()
-f.close()
-m = 0
-k = 1
-for x in range(0, len(s) - 1):
-    if s[x] == "A" and s[x + 1] == "A":
-        k += 1
-    else:
-        m = max(k, m)
-        k = 1
-print(m)
+for n in range(110203, 110246):
+    l = []
+    for delit in range(2, n + 1, 2):
+        if n % delit == 0:
+            l.append(delit)
+    if len(l) == 4:
+        print(l[0], l[1], l[2], l[3])
