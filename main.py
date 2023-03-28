7f8d1a8d1fb5582ee36950cf1b70e8bc41891bf0
Даниил <danya.45san@outlook.com>
2023-03-28 19:59:20 +0300

    Перевод числа из семеричной системы счисления в десятеричную.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\Питон\\21.02.23_5.txt")
-s = f.read()
-f.close()
-count = 0
-l = []
-for x in range(0, len(s) - 2):
-    if s[x] == s[x + 2]:
-        l.append(s[x + 1])
-l1 = set(l)
-for x in l1:
-    print(x, l.count(x))
+s = "346"
+n = 7
+def to_decime (s, n):
+    k = len(s) - 1
+    s1 = 0
+    i = 0
+    while k >= 0:
+        s1 += int(s[i]) * n ** k
+        i += 1
+        k -= 1
+    return s1
+print(to_decime('346', 7))
