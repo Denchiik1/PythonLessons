59a3a83d9be690c3bf43d442da60038016b46c64
Даниил <danya.45san@outlook.com>
2023-04-04 19:36:18 +0300

    Перевод любого числа из любой системы счисления в другую до десятеричной.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-s = "346"
-n = 7
-def to_decime (s, n):
+def to_decimal(s, n):
     k = len(s) - 1
     s1 = 0
     i = 0
@@ -9,4 +7,18 @@ def to_decime (s, n):
         i += 1
         k -= 1
     return s1
-print(to_decime('346', 7))
+def from_decime(x, n):
+    s1 = 0
+    l = []
+    while x > n:
+        x1 = x % n
+        x = x // n
+        l.append(x1)
+    l.append(x)
+    l.reverse()
+    return l
+
+def f(x, n1, n2):
+    x2 = to_decimal(x, n1)
+    return from_decime(x2, n2)
+print(f('26', 8, 3))
