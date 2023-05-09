d41afe6827aa8431431a3d4a70442049e9a64dc7
Даниил <danya.45san@outlook.com>
2023-05-09 19:13:27 +0300

    Нужно найти число, которое после данного алгоритма будет наименьшим, и будет больше 120.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-l = []
-prov = 1
-while prov > 0:
-    chislo = int(input())
-    if chislo > 9 and chislo < 100:
-        l.append(chislo)
-    prov = chislo
-obsh = 0
-for y in l:
-    obsh += y
-if obsh == 0:
-    print("NO")
-else:
-    print(obsh / len(l))
+n = 0
+r = 1
+while r <= 68:
+    n += 1
+    r = bin(n)[2:]
+    if r.count("1") % 2 == 0:
+        r += "0"
+        r = "101" + r[3:]
+    else:
+        r += "11"
+        r = "10" + r[2:]
+    r = int(r, 2)
+print(n)
