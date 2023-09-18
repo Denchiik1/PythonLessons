5e26a6a4d422866cc266c8d61d4c1a5f146c0025
Denchiik1 <danya.45san@outlook.com>
2023-09-18 18:03:14 +0300

    То же самое но в шестнадтеричную

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-a = 36
+a = 250
 s = ""
 while a > 0:
-    c = str(a % 2)
+    c = str(a % 16)
+    if c == '10':
+        c = 'A'
+    elif c == '11':
+        c = 'B'
+    elif c == '12':
+        c = 'C'
+    elif c == '13':
+        c = 'D'
+    elif c == '14':
+        c = 'E'
+    elif c == '15':
+        c = 'F'
     s = c + s
-    a = a // 2
+    a = a // 16
 print(s)
