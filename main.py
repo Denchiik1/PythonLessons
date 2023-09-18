fb57443ce4f58a34c549db7df26e10009d9aed6a
Denchiik1 <danya.45san@outlook.com>
2023-09-18 17:56:49 +0300

    Перевод числа из десятеричной системы счисления в двоичную без bin

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-f = open("C:\\Users\\danya\\OneDrive\\Рабочий стол\\abapol.csv", encoding='utf8')
-s = f.readlines()[1:]
-f.close()
-l = []
-t1 = 0
-arif = 0
-kol_vo = 0
-for x in s:
-    l.append([int(y) for y in x.split(';')[2:]])
-for x in l:
-    if x[1] == 5 and (x[2] < 4 and x[2] != 0) and (x[3] < 4 and x[3] != 0):
-        t1 += 1
-    if x[0] == 5 and x[3] != 0:
-        arif += x[3]
-        kol_vo += 1
-print(t1)
-print(round(arif/kol_vo, 2))
+a = 36
+s = ""
+while a > 0:
+    c = str(a % 2)
+    s = c + s
+    a = a // 2
+print(s)
